bc099ef839848a308bc9ad91fbbe2cbe16c302af
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-17 17:26:37 +0100

    bulbasaur get

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Switch , Route } from "react-router-dom";
 
 
 const App = () => {
-//  const [state, setstate] = useState(null)
+
   return(
     <>
       <BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react'
+
 const Home =() =>{
+
+     const [state, setState] = useState(null)
+
+     useEffect( () =>{
+         fetch('https://pokeapi.co/api/v2/pokemon/1')
+            .then(res => res.json())
+            .then(data => setState(data))
+     },[])
+     console.log(state);
+     if(state === null){
+         return(<></>)
+     }
+     console.log(state.types);
     return (
-    <p>Home</p>
-    )
+        
+        <div>
+            <img src={state.sprites.front_shiny} alt="img" />
+            <p>{state.name}</p>
+            <p>{state.height}</p>
+            <p>{state.weight}</p>
+            <>{state.types.map( type =>(<p>type : {type.type.name}</p>))}</>
+        </div>
+    
+        )
+    
 }
 
 export default Home
